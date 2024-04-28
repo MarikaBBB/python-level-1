6240f9b8f3090a3b939ca90243a8c260484337f0
Marika Bertelli <126022615+MarikaBBB@users.noreply.github.com>
2024-04-28 16:57:47 +0100

    Add files via upload

---
+++ Unit Converter.py
@@ -0,0 +1,5 @@
+print("Welcome to the Weight Converter!")
+print("This program will convert your weight from kilograms to pounds.")
+kg_num = float(input("Please enter your weight in kilograms: "))
+convert_num = kg_num * 2.20462 
+print(f"{kg_num} kg is equivalent to {convert_num} pounds.")
